8ba3fb7c498b79f5c8f844d7c01619831d3841e8
KevinFrederick <kevin55622@gmail.com>
2022-11-07 16:27:19 +0700

    GET /movie/:id

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ dotenv.config();
 
 const [port, app] = [Number(process.env.PORT), express()];
 
-app.get('/', (req, res) => {
+app.get('/movies', (req, res) => {
     const getMovieFile = fs.readFileSync('assets/tmdb_movies.json');
     const allMovies = JSON.parse(getMovieFile);
     res.send(allMovies);
 });
+
+app.get("/movie/:id", (req, res)=>{
+    const getMovieFile = fs.readFileSync('assets/tmdb_movies.json');
+    const allMovies = JSON.parse(getMovieFile);
+    allMovies.forEach(movie => {
+        if(movie["id"] === parseInt(req.params.id)){
+            res.send(movie)
+        }
+    });
+    res.status(404).json({"message" : "Movie is not found"})
+})
 
 app.listen(port, () => {
     console.log(`Server Started At Port ${port}`);
