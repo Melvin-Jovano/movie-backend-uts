816eb5bb0976028d288ba67d550041aafec6e18f
KevinFrederick <kevin55622@gmail.com>
2022-11-07 21:53:49 +0700

    PUT movie/:id

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express, { json } from "express";
 import * as dotenv from 'dotenv';
 import fs from 'fs';
+import { type } from "os";
 dotenv.config();
 
-
-
 const [port, app] = [Number(process.env.PORT), express()];
+app.use(express.json())
 
 app.get('/movies', (req, res) => {
     const getMovieFile = fs.readFileSync('assets/tmdb_movies.json');
@@ -17,8 +17,21 @@ app.get("/movie/:id", (req, res)=>{
     const getMovieFile = fs.readFileSync('assets/tmdb_movies.json');
     const allMovies = JSON.parse(getMovieFile);
     allMovies.forEach(movie => {
-        if(movie["id"] === parseInt(req.params.id)){
+        if(movie["id"] === Number(req.params.id)){
             res.send(movie)
+        }
+    });
+    res.status(404).json({"message" : "Movie is not found"})
+})
+
+app.put("/movie/:id", (req, res)=>{
+    const getMovieFile = fs.readFileSync('assets/tmdb_movies.json');
+    const allMovies = JSON.parse(getMovieFile);
+    allMovies.forEach(movie => {
+        if(movie["id"] === Number(req.params.id)){
+            movie["budget"] = req.body.budget
+            fs.writeFileSync('assets/tmdb_movies.json', JSON.stringify(allMovies))
+            res.json({"id" : movie["id"], "budget" : movie["budget"]})
         }
     });
     res.status(404).json({"message" : "Movie is not found"})
