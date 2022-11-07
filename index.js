1bf4eda3293670a0ae1ad1c3d87843bd83b397d1
mhduusop <mhduusop@gmail.com>
2022-11-07 17:49:17 +0700

    add route delete

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-import express from "express";
+import express, { json } from "express";
 import * as dotenv from 'dotenv';
 import fs from 'fs';
 dotenv.config();
 
-
-
 const [port, app] = [Number(process.env.PORT), express()];
 
-app.get('/', (req, res) => {
-    const getMovieFile = fs.readFileSync('assets/tmdb_movies.json');
-    const allMovies = JSON.parse(getMovieFile);
-    res.send(allMovies);
-});
+const getMovieFile = fs.readFileSync('assets/tmdb_movies.json');
+let allMovies = JSON.parse(getMovieFile);
+
+// delete movie
+app.delete('/movie/:id', (req, res) => {
+    let isFound = false;
+    allMovies = allMovies.filter((x => {
+        if(x.id == req.params.id) isFound = true;
+        return x.id != req.params.id;
+    }));
+    if(isFound) res.status(200).json({"message": "Movie deleted successfull", "data": allMovies});
+    else res.status(200).json({"message": "Movie is not found", "data": allMovies});
+})
 
 app.listen(port, () => {
     console.log(`Server Started At Port ${port}`);
